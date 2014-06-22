60f403dba26ab5279f9efbd30748258f93dfe94a
ogdabou <ogdabou@gmail.com>
2014-06-22 14:15:22 +0200

    Added gitignore

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Compass CSS framework config file
+
+# require 'susy'
+# require 'modular-scale'
+# require 'sassy-buttons'
+# require 'breakpoint'
+# require 'compass-css-arrow'
+# require 'rgbapng'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/LibraryBox/Content"
+sass_dir = "/sass"
+css_dir = "/LibraryBox/Content/css"
+images_dir = "/LibraryBox/Content/img"
+fonts_dir = "/LibraryBox/Content/fonts"
+javascripts_dir = "/LibraryBox/Content/js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
